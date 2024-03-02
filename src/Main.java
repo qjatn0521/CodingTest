434bcfdcfdfd95bed872636b91944b8744d18d7f
qjatn0521 <qjatn0521@kw.ac.kr>
2024-03-02 13:21:46 +0900

    [BAE] 15684(G3)
    아... 항상 실수를 놓치지 말아야한다... 왜 체크 할 필요 없는 것을 체크해서 빨리끝냈는데 더 본지 모르겠다....

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,9 @@ import java.util.Arrays;
 
 // 백준 14503 1시도 통과
 public class Main {
-    static ArrayList<Integer> arrX = new ArrayList();
-    static ArrayList<Integer> arrY = new ArrayList();
+
     static int[][] arr;
-    static int min = 65;
+    static int min = 4;
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -19,23 +18,18 @@ public class Main {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
             String[] tmp = bf.readLine().split(" ");
-            arr = new int[Integer.parseInt(tmp[0])][Integer.parseInt(tmp[1])];
-
-
-            for(int i=0;i<arr.length;i++) {
+            arr = new int[Integer.parseInt(tmp[2])][Integer.parseInt(tmp[0])];
+            int n =Integer.parseInt(tmp[1]);
+            for(int i=0;i<n;i++) {
                 tmp = bf.readLine().split(" ");
-                for(int j=0;j<arr[0].length;j++) {
-                    arr[i][j] = Integer.parseInt(tmp[j]);
-                    if(arr[i][j]!=0 && arr[i][j]!=6) {
-                        arrX.add(j);
-                        arrY.add(i);
-                    }
-                }
+                arr[Integer.parseInt(tmp[0])-1][Integer.parseInt(tmp[1])-1] = 1;
+                arr[Integer.parseInt(tmp[0])-1][Integer.parseInt(tmp[1])] = -1;
             }
             fun0(0);
             for(int i=0;i<arr.length;i++) {
-                bw.write(Arrays.toString(arr[i])+"\n");
+                //bw.write(Arrays.toString(arr[i])+"\n");
             }
+            if(min >=4) min = -1;
             bw.write(""+min);
             bw.flush();
             bw.close();
@@ -46,152 +40,34 @@ public class Main {
     }
 
     static void fun0(int n) {
-        if(n==arrX.size()) {
-            fun5();
-            return;
-        }
-        int x = arrX.get(n);
-        int y = arrY.get(n);
-        int num = arr[y][x];
-        if(num==1) {
-            fun1(x,y,true);
-            fun0(n+1);
-            fun1(x,y,false);
-            fun2(x,y,true);
-            fun0(n+1);
-            fun2(x,y,false);
-            fun3(x,y,true);
-            fun0(n+1);
-            fun3(x,y,false);
-            fun4(x,y,true);
-            fun0(n+1);
-            fun4(x,y,false);
-        } else if(num==2) {
-            fun1(x,y,true);
-            fun2(x,y,true);
-            fun0(n+1);
-            fun1(x,y,false);
-            fun2(x,y,false);
-            fun3(x,y,true);
-            fun4(x,y,true);
-            fun0(n+1);
-            fun3(x,y,false);
-            fun4(x,y,false);
-        } else if(num==3) {
-            fun1(x,y,true);
-            fun3(x,y,true);
-            fun0(n+1);
-            fun1(x,y,false);
-            fun3(x,y,false);
-            fun1(x,y,true);
-            fun4(x,y,true);
-            fun0(n+1);
-            fun1(x,y,false);
-            fun4(x,y,false);
-            fun2(x,y,true);
-            fun3(x,y,true);
-            fun0(n+1);
-            fun2(x,y,false);
-            fun3(x,y,false);
-            fun2(x,y,true);
-            fun4(x,y,true);
-            fun0(n+1);
-            fun2(x,y,false);
-            fun4(x,y,false);
-        } else if(num==4) {
-            fun1(x,y,true);
-            fun2(x,y,true);
-            fun3(x,y,true);
-            fun0(n+1);
-            fun1(x,y,false);
-            fun2(x,y,false);
-            fun3(x,y,false);
-            fun1(x,y,true);
-            fun2(x,y,true);
-            fun4(x,y,true);
-            fun0(n+1);
-            fun1(x,y,false);
-            fun2(x,y,false);
-            fun4(x,y,false);
-            fun2(x,y,true);
-            fun3(x,y,true);
-            fun4(x,y,true);
-            fun0(n+1);
-            fun2(x,y,false);
-            fun3(x,y,false);
-            fun4(x,y,false);
-            fun1(x,y,true);
-            fun3(x,y,true);
-            fun4(x,y,true);
-            fun0(n+1);
-            fun1(x,y,false);
-            fun3(x,y,false);
-            fun4(x,y,false);
-        } else if(num==5) {
-            fun1(x,y,true);
-            fun2(x,y,true);
-            fun3(x,y,true);
-            fun4(x,y,true);
-            fun0(n+1);
-            fun1(x,y,false);
-            fun2(x,y,false);
-            fun3(x,y,false);
-            fun4(x,y,false);
+        boolean pass = fun1();
+        if(pass) {
+            if(n<min)
+                min =n;
+        } else if(n<3) {
+            for(int i=0;i<arr.length;i++) {
+                for(int j=0;j<arr[0].length-1;j++) {
+                    if(arr[i][j]==0&&arr[i][j+1]==0) {
+                        arr[i][j]=1;
+                        arr[i][j+1]=-1;
+                        fun0(n+1);
+                        arr[i][j+1]=0;
+                        arr[i][j]=0;
+                    }
+                }
+            }
         }
     }
 
-    static void fun1(int x, int y,boolean option) {
-        //오른쪽
-        while(x<arr[0].length && arr[y][x]!=6) {
-            if(arr[y][x]<=0) {
-                if(option) arr[y][x]--;
-                else arr[y][x]++;
+    static boolean fun1() {
+        for(int i=0;i<arr[0].length;i++) {
+            int x = i;
+            for(int j=0;j<arr.length;j++) {
+                if(arr[j][x]==1) x++;
+                else if(arr[j][x]==-1) x--;
             }
-            x++;
+            if(x!=i) return false;
         }
-    }
-    static void fun2(int x, int y,boolean option) {
-        //왼쪽
-        while(x>=0 && arr[y][x]!=6) {
-            if(arr[y][x]<=0) {
-                if(option) arr[y][x]--;
-                else arr[y][x]++;
-            }
-            x--;
-        }
-    }
-    static void fun3(int x, int y,boolean option) {
-        //아래
-        while(y<arr.length && arr[y][x]!=6) {
-            if(arr[y][x]<=0) {
-                if(option) arr[y][x]--;
-                else arr[y][x]++;
-            }
-            y++;
-        }
-    }
-    static void fun4(int x, int y,boolean option) {
-        //위
-        while(y>=0 && arr[y][x]!=6) {
-            if(arr[y][x]<=0) {
-                if(option) arr[y][x]--;
-                else arr[y][x]++;
-            }
-            y--;
-        }
-    }
-    static void fun5() {
-        int tmp = 0;
-        for(int i=0;i<arr.length;i++) {
-            System.out.print(Arrays.toString(arr[i])+"\n");
-        }
-
-        for(int i=0;i<arr.length;i++) {
-            for(int j=0;j<arr[0].length;j++) {
-                if(arr[i][j]==0) tmp++;
-            }
-        }
-        if(tmp<min) min = tmp;
-        System.out.println("ans :"+tmp+"\n");
+        return true;
     }
 }
