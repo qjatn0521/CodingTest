7f9c5a733a87d64af1bee8a58e8492216df3602e
qjatn0521 <qjatn0521@kw.ac.kr>
2024-03-03 18:50:36 +0900

    [BAE] 15685(G3)
    이번건 so EASY 했습니다 30 분 컷

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class Main {
 
     static int[][] arr;
-    static int min = 4;
+    static ArrayList<Integer> directions = new ArrayList<>();
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -18,19 +18,41 @@ public class Main {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
             String[] tmp = bf.readLine().split(" ");
-            arr = new int[Integer.parseInt(tmp[2])][Integer.parseInt(tmp[0])];
-            int n =Integer.parseInt(tmp[1]);
+            arr = new int[101][101];
+            int n =Integer.parseInt(tmp[0]);
             for(int i=0;i<n;i++) {
                 tmp = bf.readLine().split(" ");
-                arr[Integer.parseInt(tmp[0])-1][Integer.parseInt(tmp[1])-1] = 1;
-                arr[Integer.parseInt(tmp[0])-1][Integer.parseInt(tmp[1])] = -1;
+                directions.clear();
+                int tmpDir = Integer.parseInt(tmp[2]);
+                int tmpX = Integer.parseInt(tmp[0]);
+                int tmpY = Integer.parseInt(tmp[1]);
+                directions.add(tmpDir);
+                arr[tmpY][tmpX]++;
+                if(tmpDir == 0) {
+                    arr[tmpY][tmpX+1]++;
+                    tmpX+=1;
+                } else if(tmpDir == 1) {
+                    arr[tmpY-1][tmpX]++;
+                    tmpY-=1;
+                } else if(tmpDir == 2) {
+                    arr[tmpY][tmpX-1]++;
+                    tmpX-=1;
+                } else {
+                    arr[tmpY+1][tmpX]++;
+                    tmpY+=1;
+                }
+                fun0(tmpX,tmpY,Integer.parseInt(tmp[3]));
             }
-            fun0(0);
-            for(int i=0;i<arr.length;i++) {
+            int ans = 0;
+            for(int i=0;i<arr.length-1;i++) {
                 //bw.write(Arrays.toString(arr[i])+"\n");
+                for(int j=0;j<arr.length-1;j++) {
+                    if(arr[i][j]>0 && arr[i][j+1]>0 && arr[i+1][j]>0 && arr[i+1][j+1]>0) {
+                        ans++;
+                    }
+                }
             }
-            if(min >=4) min = -1;
-            bw.write(""+min);
+            bw.write(""+ans);
             bw.flush();
             bw.close();
         }catch(IOException e){
@@ -39,35 +61,38 @@ public class Main {
         }
     }
 
-    static void fun0(int n) {
-        boolean pass = fun1();
-        if(pass) {
-            if(n<min)
-                min =n;
-        } else if(n<3) {
-            for(int i=0;i<arr.length;i++) {
-                for(int j=0;j<arr[0].length-1;j++) {
-                    if(arr[i][j]==0&&arr[i][j+1]==0) {
-                        arr[i][j]=1;
-                        arr[i][j+1]=-1;
-                        fun0(n+1);
-                        arr[i][j+1]=0;
-                        arr[i][j]=0;
-                    }
-                }
+    static void fun0(int x,int y, int age) {
+        //System.out.println("\nx :"+x+",y : "+y+", age : "+age);
+        if(age<=0) return;
+        int curSize = directions.size();
+        for(int i=curSize-1;i>=0;i--) {
+            int tmpDir = directions.get(i);
+            if(tmpDir == 0) {
+                arr[y-1][x]++;
+                y-=1;
+                tmpDir+=1;
+            } else if(tmpDir == 1) {
+                arr[y][x-1]++;
+                x-=1;
+                tmpDir+=1;
+            } else if(tmpDir == 2) {
+                arr[y+1][x]++;
+                y+=1;
+                tmpDir+=1;
+            } else {
+                arr[y][x+1]++;
+                x+=1;
+                tmpDir=0;
             }
+            directions.add(tmpDir);
         }
+        fun0(x,y,age-1);
     }
 
-    static boolean fun1() {
-        for(int i=0;i<arr[0].length;i++) {
-            int x = i;
-            for(int j=0;j<arr.length;j++) {
-                if(arr[j][x]==1) x++;
-                else if(arr[j][x]==-1) x--;
-            }
-            if(x!=i) return false;
-        }
-        return true;
-    }
 }
+/*
+3
+3 3 0 2
+4 2 1 3
+4 2 2 1
+ */
