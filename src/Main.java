9b3277d72958d9bda5ff469f234cd9557563580e
qjatn0521 <qjatn0521@kw.ac.kr>
2024-03-06 15:53:11 +0900

    [BAE] 16235(G3)
    하... 힘들었다. 아직 시간 관리랑 메모리 관리는 많이 안해봐서 항상 유의하면서 해야 할듯

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-// 2 : 13
+// 12:20
 public class Main {
     static int[][] arr;
-    static int[][] gone;
-    static int num = 1;
-    static int l;
-    static int r;
+    static int[][] feed;
+    static int[][] treesY = new int[10000][100];
+    static  int[][] treesX = new int[10000][100];
+    static int[] num = new int[10000];
+    static ArrayList<Integer> deadTree = new ArrayList<>();
+    static ArrayList<Integer> deadTreeY = new ArrayList<>();
+    static ArrayList<Integer> deadTreeX = new ArrayList<>();
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -21,19 +26,38 @@ public class Main {
 
             String[] tmp = bf.readLine().split(" ");
             int n = Integer.parseInt(tmp[0]);
-            l = Integer.parseInt(tmp[1]);
-            r = Integer.parseInt(tmp[2]);
+            int n2 = Integer.parseInt(tmp[1]);
+            int year = Integer.parseInt(tmp[2]);
             arr = new int[n][n];
-            gone = new int[n][n];
+            feed = new int[n][n];
             for(int i=0;i<n;i++) {
                 tmp = bf.readLine().split(" ");
                 for(int j=0;j<n;j++) {
-                    arr[i][j] = Integer.parseInt(tmp[j]);
+                    arr[i][j] = 5;
+                    feed[i][j] = Integer.parseInt(tmp[j]);
                 }
             }
-            int ans = fun1(0);
+            for(int i=1;i<num.length;i++) {
+            }
+            for(int i=0;i<n2;i++) {
+                tmp = bf.readLine().split(" ");
+                treesY[Integer.parseInt(tmp[2])][num[Integer.parseInt(tmp[2])]] = (Integer.parseInt(tmp[0])-1);
+                treesX[Integer.parseInt(tmp[2])][num[Integer.parseInt(tmp[2])]] = (Integer.parseInt(tmp[1])-1);
+                num[Integer.parseInt(tmp[2])]++;
+            }
+            for(int i=0;i<year;i++) {
+                fun1();
+                for(int j=0;j<n;j++) {
+                    //bw.write(Arrays.toString(arr[j])+"\n");
+                }
+                //bw.write("\n");
+            }
+            int ans = 0;
+
+            for(int i =1;i<num.length;i++) {
+                ans+= num[i];
+            }
             bw.write(""+ans);
-            //bw.write(Arrays.deepToString(arr));
             bw.flush();
             bw.close();
         }catch(IOException e){
@@ -41,71 +65,126 @@ public class Main {
         }
     }
 
-    static int fun1(int n) {
-
-        //System.out.println("arr : "+Arrays.deepToString(arr));
-        for(int i=0;i<gone.length;i++) {
-            for(int j=0;j<gone.length;j++) {
-                if(gone[i][j]==0) {
-                    fun2(i,j);
-                    num++;
-                }
-                //System.out.println(Arrays.deepToString(gone));
-            }
-        }
-        //System.out.println();
-        if(num== gone.length* gone.length+1) {
-            return n;
-        }else {
-            fun3();
-            num=1;
-            return fun1(n+1);
-        }
+    static void fun1() {
+        fun2();
+        fun3();
+        fun4();
+        fun5();
     }
 
-    private static void fun2(int y, int x) {
-        gone[y][x] = num;
-        if(x+1<gone.length && gone[y][x+1]==0
-                && Math.abs(arr[y][x] - arr[y][x+1])>=l &&Math.abs(arr[y][x] - arr[y][x+1])<=r) {
-            gone[y][x+1] = num;
-            fun2(y,x+1);
-        }
-        if(x>0 && gone[y][x-1]==0
-                && Math.abs(arr[y][x] - arr[y][x-1])>=l &&Math.abs(arr[y][x] - arr[y][x-1])<=r){
-            gone[y][x-1] = num;
-            fun2(y,x-1);
-        }
-        if(y+1< gone.length && gone[y+1][x]==0
-                && Math.abs(arr[y][x] - arr[y+1][x])>=l &&Math.abs(arr[y][x] - arr[y+1][x])<=r){
-            gone[y+1][x] = num;
-            fun2(y+1,x);
-        }
-        if(y>0 && gone[y-1][x]==0
-                && Math.abs(arr[y][x] - arr[y-1][x])>=l &&Math.abs(arr[y][x] - arr[y-1][x])<=r){
-            gone[y-1][x] = num;
-            fun2(y-1,x);
+    static void fun2() {
+        Queue<Integer> tmpListY = new LinkedList<>();
+        Queue<Integer> tmpListX = new LinkedList<>();
+        for(int i =1;i<num.length;i++) {
+            int size = 0;
+            for(int j=0;j<num[i];j++) {
+                int tmpY = treesY[i][j];
+                int tmpX = treesX[i][j];
+                if(!(tmpY<0 || tmpX<0 || tmpY>= arr.length || tmpX>= arr.length)) {
+                    if(arr[tmpY][tmpX]>=i) {
+                        arr[tmpY][tmpX]-=i;
+                        size++;
+                        tmpListY.add(tmpY);
+                        tmpListX.add(tmpX);
+                    } else {
+                        deadTree.add(i);
+                        deadTreeY.add(tmpY);
+                        deadTreeX.add(tmpX);
+                    }
+                }
+            }
+            num[i] = tmpListY.size()-size;
+            treesY[i] = new int[num[i]];
+            treesX[i] = new int[num[i]];
+            for(int j=0;j<num[i];j++) {
+                int tmpY = tmpListY.peek();
+                int tmpX = tmpListX.peek();
+                treesY[i][j] = tmpY;
+                treesX[i][j] = tmpX;
+                tmpListY.poll();
+                tmpListX.poll();
+            }
         }
     }
     static void fun3() {
-        int[] sum = new int[num];
-        int[] how = new int[num];
-        for(int i=0;i<gone.length;i++) {
-            for(int j=0;j<gone.length;j++) {
-                if(gone[i][j]>0) {
-                    sum[gone[i][j]-1]+= arr[i][j];
-                    how[gone[i][j]-1]++;
+        for(int i=0;i<deadTree.size();i++) {
+            int age = deadTree.get(i);
+            arr[deadTreeY.get(i)][deadTreeX.get(i)] += (age/2);
+        }
+        deadTree.clear();
+        deadTreeX.clear();
+        deadTreeY.clear();
+    }
+    static void fun4() {
+        Queue<Integer> tmpListY = new LinkedList<>();
+        Queue<Integer> tmpListX = new LinkedList<>();
+        for(int i=5;i<num.length;i+=5) {
+            for(int j=0;j<num[i];j++) {
+                int tmpY = treesY[i][j];
+                int tmpX = treesX[i][j];
+                if(tmpY-1>=0 && tmpX-1>=0) {
+                    tmpListY.add(tmpY-1);
+                    tmpListX.add(tmpX-1);
                 }
+                if(tmpY-1>=0) {
+                    tmpListY.add(tmpY-1);
+                    tmpListX.add(tmpX);
+                }
+                if(tmpY-1>=0 && tmpX+1<arr.length) {
+                    tmpListY.add(tmpY-1);
+                    tmpListX.add(tmpX+1);
+                }
+                if(tmpX-1>=0) {
+                    tmpListY.add(tmpY);
+                    tmpListX.add(tmpX-1);
+                }
+                if(tmpX+1<arr.length) {
+                    tmpListY.add(tmpY);
+                    tmpListX.add(tmpX+1);
+                }
+                if(tmpY+1<arr.length && tmpX-1>=0) {
+                    tmpListY.add(tmpY+1);
+                    tmpListX.add(tmpX-1);
+                }
+                if(tmpY+1<arr.length) {
+                    tmpListY.add(tmpY+1);
+                    tmpListX.add(tmpX);
+                }
+                if(tmpY+1<arr.length && tmpX+1<arr.length) {
+                    tmpListY.add(tmpY+1);
+                    tmpListX.add(tmpX+1);
+                }
+                //System.out.println("num 1 : "+num[1]+", "+i+", "+tmpY+", "+tmpX);
             }
         }
-        for(int i=0;i<gone.length;i++) {
-            for(int j=0;j<gone.length;j++) {
-                for(int k=0;k<num;k++) {
-                    if(gone[i][j]-1==k) {
-                        arr[i][j] = sum[k]/how[k];
-                    }
-                }
-                gone[i][j]=0;
+
+        num[1] = tmpListY.size();
+        treesY[1] = new int[num[1]];
+        treesX[1] = new int[num[1]];
+        for(int i=0;i<num[1];i++) {
+            int tmpY = tmpListY.peek();
+            int tmpX = tmpListX.peek();
+            treesY[1][i] = tmpY;
+            treesX[1][i] = tmpX;
+            tmpListY.poll();
+            tmpListX.poll();
+        }
+    }
+    static void fun5() {
+        for(int i=0;i<arr.length;i++) {
+            for(int j=0;j<arr.length;j++) {
+                arr[i][j] += feed[i][j];
             }
         }
     }
 }
+/*
+5 2 2
+2 3 2 3 2
+2 3 2 3 2
+2 3 2 3 2
+2 3 2 3 2
+2 3 2 3 2
+1 5 3
+3 2 3
+ */
