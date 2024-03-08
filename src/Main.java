3abb316ce7402f18288f1b9415818bd092701ed8
we_BS <qjatn0521@kw.ac.kr>
2024-03-08 14:09:15 +0900

    [BAE] 17144(G4)
    So Easy

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,8 @@ import java.util.Queue;
 // 10:18 ~ 11 : 24,
 public class Main {
     static int[][] arr;
-    static int[][] gone;
-    static int sharkY = 0;
-    static int sharkX = 0;
-    static int size = 2;
-    static int feed = 0;
+    static int[][] plusDust;
+    static int cleanerY = 0;
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -23,24 +20,28 @@ public class Main {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
             String[] tmp = bf.readLine().split(" ");
-            int n = Integer.parseInt(tmp[0]);
-            arr = new int[n][n];
-            for(int i=0;i<n;i++) {
+            arr = new int[Integer.parseInt(tmp[0])][Integer.parseInt(tmp[1])];
+            plusDust = new int[Integer.parseInt(tmp[0])][Integer.parseInt(tmp[1])];
+            int sec = Integer.parseInt(tmp[2]);
+            for(int i=0;i<arr.length;i++) {
                 tmp = bf.readLine().split(" ");
-                for(int j=0;j<n;j++) {
+                for(int j=0;j<arr[0].length;j++) {
                     int num = Integer.parseInt(tmp[j]);
-                    if(num == 9) {
-                        sharkY = i;
-                        sharkX = j;
-                    } else {
-                        arr[i][j] = Integer.parseInt(tmp[j]);
+                    if(num==-1) {
+                        cleanerY = i;
                     }
+                    arr[i][j] = num;
+                }
+            }
+            fun1(sec);
+            int sum = 2;
+            for(int i=0;i<arr.length;i++){
+                for(int j=0;j<arr[0].length;j++) {
+                    sum+=arr[i][j];
                 }
             }
 
-            int ans = fun1(0);
-
-            bw.write(""+ans);
+            bw.write(""+sum);
             bw.flush();
             bw.close();
         }catch(IOException e){
@@ -48,50 +49,75 @@ public class Main {
         }
     }
 
-    static int fun1(int len) {
-        int tmpY=-1;
-        int tmpX=-1;
-        int tmpLength = 100000;
-        fun3();
+    static void fun1(int sec) {
+        if(sec<=0) return;
+        fun2();
+        fun4();
+        fun1(sec-1);
+    }
+
+    static void fun2() {
         for(int i=0;i<arr.length;i++) {
-            for(int j=0;j<arr.length;j++) {
-                if(arr[i][j]!=0 && arr[i][j]<size && tmpLength>gone[i][j]-1 &&gone[i][j] !=0) {
-                    tmpY = i;
-                    tmpX = j;
-                    tmpLength = gone[i][j]-1;
-                }
+            for(int j=0;j<arr[0].length;j++) {
+                if(arr[i][j]>0) fun3(i,j);
             }
         }
-        if(tmpY==-1) {
-            return len;
-        } else {
-            fun2(tmpY,tmpX);
-            return fun1(len+tmpLength);
+        for(int i=0;i<arr.length;i++) {
+            for(int j=0;j<arr[0].length;j++) {
+                arr[i][j] += plusDust[i][j];
+                plusDust[i][j]=0;
+            }
         }
     }
-
-    static void fun2(int y, int x) {
-        sharkX = x;
-        sharkY = y;
-        arr[y][x] = 0;
-        feed++;
-        if(feed==size) {
-            feed=0;
-            size++;
+    static void fun3(int y, int x) {
+        int value = arr[y][x]/5;
+        if(x-1>=0 && arr[y][x-1]>=0) {
+            plusDust[y][x-1] +=value;
+            arr[y][x] -=value;
+        }
+        if(y-1>=0 && arr[y-1][x]>=0) {
+            plusDust[y-1][x] +=value;
+            arr[y][x] -=value;
+        }
+        if(x+1<arr[0].length) {
+            plusDust[y][x+1] +=value;
+            arr[y][x] -=value;
+        }
+        if(y+1<arr.length && arr[y+1][x]>=0) {
+            plusDust[y+1][x] +=value;
+            arr[y][x] -=value;
         }
     }
-    static void fun3() {
-        gone = new int [arr.length][arr.length];
-        fun4(sharkY,sharkX,1);
-    }
+    static void fun4() {
+        funDown(0,cleanerY-3,0);
+        funUp(0, cleanerY+2,arr.length-1);
+        funLeft(0);
+        funLeft(arr.length-1);
+        funUp(arr[0].length-1, 1,cleanerY-1);
+        funDown(arr[0].length-1,arr.length-2,cleanerY);
+        funRight(cleanerY-1);
+        funRight(cleanerY);
 
-    static void fun4(int sY, int sX, int num) {
-        if(gone[sY][sX]==0||gone[sY][sX]>num) {
-            gone[sY][sX] = num;
-            if(sY-1>=0 && arr[sY-1][sX]<=size) fun4(sY-1,sX,num+1);
-            if(sX-1>=0&& arr[sY][sX-1]<=size) fun4(sY,sX-1,num+1);
-            if(sY+1<arr.length&& arr[sY+1][sX]<=size) fun4(sY+1,sX,num+1);
-            if(sX+1<arr.length&& arr[sY][sX+1]<=size) fun4(sY,sX+1,num+1);
+    }
+    static void funRight(int y) {
+        for(int i=arr[0].length-2;i>0;i--) {
+            arr[y][i+1] = arr[y][i];
+        }
+        arr[y][1]=0;
+    }
+    static void funDown(int x, int startY, int endY) {
+        for(int i=startY;i>=endY;i--) {
+            arr[i+1][x] = arr[i][x];
+        }
+    }
+    static void funLeft(int y) {
+        for(int i=1;i<arr[0].length;i++) {
+            arr[y][i-1] = arr[y][i];
+        }
+    }
+    static void funUp(int x, int startY, int endY) {
+        for(int i=startY;i<=endY;i++) {
+            arr[i-1][x] = arr[i][x];
         }
     }
 }
